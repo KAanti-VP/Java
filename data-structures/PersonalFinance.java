391f188480e9653682951cadbd45cd28cacbf260
KAnti-VP <144044786+KAnti-VP@users.noreply.github.com>
2024-05-06 10:24:07 +0200

    practice

---
+++ data-structures/PersonalFinance.java
@@ -0,0 +1,22 @@
+public class PersonalFinance {
+    //We are going to represent our expenses in a list containing integers.
+    //
+    //Create a list with the following items:
+    // 500, 1000, 1250, 175, 800 and 120
+    //Create an application which prints out the answers to the following questions:
+    // - How much did we spend?
+    // - Which was our greatest expense?
+    // - Which was our cheapest spending?
+    // - What was the average amount of our spendings? (print this as a float value)
+    //The full output of your main method should be the following:
+    //
+    //3845
+    //1250
+    //120
+    //640.8333
+
+    public static void main(String[] args) {
+
+    }
+
+}
