8e5d551049d505053e7267cea35b03cb33daa831
KAnti-VP <144044786+KAnti-VP@users.noreply.github.com>
2024-05-13 10:06:31 +0200

    HashMap

---
+++ data-structures/MapIntroduction1.java
@@ -0,0 +1,55 @@
+public class MapIntroduction1 {
+    public static void main(String[] args) {
+
+        //We are going to play with maps. Feel free to use the built-in methods where possible.
+        //
+        //Create an empty map where the keys are integers and the values are characters
+
+
+        //Print out whether the map is empty or not
+
+
+        //Add the following key-value pairs to the map
+        //97 : a, 98 : b, 99 : c, 65 : A, 66 : B, 67 : C
+
+
+        //Print all the keys
+
+
+        //Print all the values
+
+
+        //Add value D with the key 68
+
+
+        //Print how many key-value pairs are in the map
+
+
+        //Print the value that is associated with key 99
+
+
+        //Remove the key-value pair with key 97 and print the associated value
+
+
+        //Print whether there is an associated value with key 100 or not
+
+
+        //Remove all the key-value pairs
+
+
+        //Print how many key-value pairs are in the map
+
+
+    }
+
+    // Output:
+    // true
+    // [97, 65, 98, 66, 99, 67]
+    // [a, A, b, B, c, C]
+    // 7
+    // c
+    // a
+    // false
+    // 0
+
+}
